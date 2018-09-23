ce5f6c9f6e5c4fe257499dbaecbf3a603695b734
Michael Friedman <frimicc@gmail.com>
2018-09-23 13:32:24 -0700

    Detail view controller layout -- scroll not working.

--- SwiftFlicks/DetailViewController.swift
+++ SwiftFlicks/DetailViewController.swift
@@ -12,13 +12,19 @@ import AlamofireImage
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var scroller: UIScrollView!
+    @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
 
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = detailItem {
+            if let label = titleLabel {
+                label.text = detail.title
+            }
             if let label = detailDescriptionLabel {
                 label.text = detail.description
             }
@@ -28,6 +34,11 @@ class DetailViewController: UIViewController {
                 } else {
                     bgImageView.image = Image(named: "NoPosterImage")
                 }
+            }
+            if let card = cardView {
+                detailDescriptionLabel.sizeToFit()
+                card.sizeToFit()
+                scroller.sizeToFit()
             }
         }
     }
