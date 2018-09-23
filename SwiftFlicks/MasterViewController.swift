5db1ed198daec2f540272e5d5864d6826c596fa3
Michael Friedman <frimicc@gmail.com>
2018-09-23 12:46:05 -0700

    Put back correct segue.

--- SwiftFlicks/MasterViewController.swift
+++ SwiftFlicks/MasterViewController.swift
@@ -70,16 +70,6 @@ class MasterViewController: UITableViewController {
         // Return false if you do not want the specified item to be editable.
         return false
     }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
-    }
-
 }
 
 extension MasterViewController: UISearchBarDelegate {
